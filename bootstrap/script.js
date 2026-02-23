8710a3770f66ed5988c4204b32a070432577e2b9
Davi Severino <daviseverinoinf2@gmail.com>
2026-02-23 11:25:21 -0300

    bootstrap

--- bootstrap/script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let btn = document.querySelector('#btn.btn-primary');
-
-    btn.addEventListener('click', function () {
-        btn.classList.toggle('right');
-    });
-});
