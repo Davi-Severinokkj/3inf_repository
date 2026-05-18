6d84791b6b9001312098891d229ca343f7d82b6f
Davi Severino Oliveira de Souza <daviseverinoinf2@gmail.com>
2026-05-18 10:19:38 -0300

    Create arrays.php

---
+++ php e mysql_2°_trimestre/aulaPHP2°Trimestre/Aula_Arrays/arrays.php
@@ -0,0 +1,16 @@
+<?php
+echo "Meu array <br>";
+$meuArray = array('Volvo' , 15 , ['apples', 'bananas']);
+var_dump($meuArray);
+echo "<br>";
+
+echo "Meu segundo array. <br>";
+$meuSegundoArray = ['Volvo' , 15 , ['apples', 'bananas']];
+var_dump($meuSegundoArray);
+echo "<br>";
+
+$names = [
+    "Jhon",
+    "Jane",
+    "Pablo"
+];
